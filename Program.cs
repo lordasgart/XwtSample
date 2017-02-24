19f41d10a99c5ca3fa5093613234d48c919ecaf4
Markus Malz <asgart79@gmail.com>
2017-02-24 18:57:58 +0100

    Use Current link and Exit on window.Close

--- Program.cs
+++ Program.cs
@@ -3,29 +3,31 @@ using Xwt;
 
 namespace XwtSample
 {
-    class MainClass
-    {
-        [STAThread]
-        public static void Main(string[] args)
-        {
-                
+	class MainClass
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+
 #if __MonoCS__
             ObjCRuntime.Dlfcn.dlopen("/Library/Frameworks/Xamarin.Mac.framework/Versions/Current/lib/libxammac.dylib", 0);
             Application.Initialize(ToolkitType.XamMac);
 #else
-            Application.Initialize(ToolkitType.Wpf);
+			Application.Initialize(ToolkitType.Wpf);
 #endif
 
-            var mainWindow = new Window()
-            {
-                Title = "XwtSample",
-                Width = 400,
-                Height = 300
-            };
+			var mainWindow = new Window()
+			{
+				Title = "XwtSample",
+				Width = 400,
+				Height = 300
+			};
 
-            mainWindow.Show();
+			mainWindow.Closed += (sender, e) => Application.Exit();
 
-            Application.Run();
-        }
-    }
+			mainWindow.Show();
+
+			Application.Run();
+		}
+	}
 }
